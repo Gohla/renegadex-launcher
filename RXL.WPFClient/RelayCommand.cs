2c600c7f09b3a4fe2bd350cf1c6fe1d39ea859cc
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-01 10:45:16 +0100

    Simple datagrid with servers in WPF client.

---
+++ RXL.WPFClient/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RXL.WPFClient
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Predicate<Object> _canExecute;
+        private readonly Action<Object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Predicate<Object> canExecute, Action<Object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(Object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(Object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void NotifyCanExecuteChanged(Object parameter)
+        {
+            if(CanExecuteChanged != null)
+                CanExecuteChanged.Invoke(parameter, new EventArgs());
+        }
+    }
+}
---
+++ RXL.WPFClient/ServersViewModel.cs
@@ -0,0 +1,30 @@
+﻿using RXL.Core;
+using RXL.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RXL.WPFClient
+{
+    public class ServersViewModel
+    {
+        private readonly ServerList _serverList;
+
+        public IObservableCollection<Server> Servers { get { return _serverList.Servers; } }
+
+        public ICommand Refresh { get; private set; }
+        public ICommand Ping { get; private set; }
+
+        public ServersViewModel()
+        {
+            _serverList = new ServerList();
+            _serverList.Refresh();
+
+            Refresh = new RelayCommand(_ => true, _ => _serverList.Refresh());
+            Ping = new RelayCommand(_ => true, _ => _serverList.Ping());
+        }
+    }
+}
