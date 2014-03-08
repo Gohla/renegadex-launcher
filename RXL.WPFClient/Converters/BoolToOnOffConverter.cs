3d6d1bb5151ce54d0f289713c759baf0160bbf19
Xciles <xciles@gmail.com>
2014-03-08 23:40:32 +0100

    Server details layout

---
+++ RXL.WPFClient/Converters/BoolToOnOffConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RXL.WPFClient.Converters
+{
+    public class BoolToOnOffConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tmp = (bool)value;
+
+            return tmp ? "On" : "Off";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
