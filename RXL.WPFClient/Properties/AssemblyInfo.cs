da8d1a473232457261f15a21ef5308888b8d6aec
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-13 21:09:06 +0100

    Update assembly information, changed version to 0.1.

--- RXL.WPFClient/Properties/AssemblyInfo.cs
+++ RXL.WPFClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RXL.WPFClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Alternative RenegadeX Launcher")]
+[assembly: AssemblyDescription("Alternative RenegadeX Launcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RXL.WPFClient")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("RXL")]
+[assembly: AssemblyCopyright("Copyright © Gabriël Konat, Hans Harts 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
