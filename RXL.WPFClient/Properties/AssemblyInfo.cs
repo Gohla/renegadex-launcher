54becbd077b4d346b531cb5495f119865fff32e1
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-18 21:02:51 +0100

    Add installer script

--- RXL.WPFClient/Properties/AssemblyInfo.cs
+++ RXL.WPFClient/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alternative RenegadeX Launcher")]
-[assembly: AssemblyDescription("Alternative RenegadeX Launcher")]
+[assembly: AssemblyTitle("RenegadeX Alternative Launcher")]
+[assembly: AssemblyDescription("RenegadeX Alternative Launcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RXL")]
