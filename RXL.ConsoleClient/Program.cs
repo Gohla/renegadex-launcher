627b9054a1491145be4370331aa8a7c8321c0604
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-19 22:30:36 +0100

    Add ReactiveIRC and dependencies. Add usage example to console client. Update reactive extensions packages.

--- RXL.ConsoleClient/Program.cs
+++ RXL.ConsoleClient/Program.cs
@@ -1,6 +1,9 @@
-﻿using RXL.Core;
+﻿using ReactiveIRC.Client;
+using ReactiveIRC.Interface;
+using RXL.Core;
 using System;
 using System.Linq;
+using System.Reactive.Linq;
 
 namespace RXL.ConsoleClient
 {
@@ -12,15 +15,25 @@ namespace RXL.ConsoleClient
 
             var servers = serverList.Refresh();
             servers.Wait();
-            foreach(Server server in servers.Result)
+            foreach (Server server in servers.Result)
             {
                 Console.WriteLine(server.Name);
             }
 
             var pings = serverList.Ping(servers.Result.Select(s => s.Address));
             pings.Wait();
-            foreach(PingResult result in pings.Result)
+            foreach (PingResult result in pings.Result)
+            {
+                if (result == null)
+                    continue;
                 Console.WriteLine("{0}: {1}", result.Reply.Status, result.Address);
+            }
+
+            IClient ircClient = new Client();
+            IClientConnection connection = ircClient.CreateClientConnection("irc.freenode.net", 6666, null);
+            connection.Connect().Wait();
+            connection.Login("Harvester", "Harvester", "Harvester").Wait();
+            connection.Join("#tz");
 
             Console.ReadKey();
         }
