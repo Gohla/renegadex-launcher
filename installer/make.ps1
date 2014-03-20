99a356db48c0d2df5b1a24f3122f79273936f147
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-20 23:04:05 +0100

    Parameterize script by optional build number. MSI installers ignore the fourth version identifier, need to use third version identifier for builds.

--- installer/make.ps1
+++ installer/make.ps1
@@ -1,6 +1,15 @@
+ param (
+    [string]$build
+ )
+
 $launcherExe = "../RXL.WPFClient/bin/Release/RXL.exe"
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($launcherExe).FileVersion
-$version = $version.Remove($version.LastIndexOf('.'))
+$version = $version.Remove($version.LastIndexOf('.'));
+$version = $version.Remove($version.LastIndexOf('.'));
+
+if($build) {
+  $version = $version + "." + $build	
+}
 
 "Building installer, setting version to " + $version + "."
 
@@ -9,6 +18,6 @@ Remove-Item -Force -Recurse "out" -ea SilentlyContinue
 Remove-Item -Force -Recurse "setup-cache" -ea SilentlyContinue
 
 $advInstallerExe = "C:/Program Files (x86)/Caphyon/Advanced Installer 11.0/bin/x86/advinst.exe"
-
 Start-Process -NoNewWindow -Wait $advInstallerExe ("/edit setup.aip /SetVersion " + $version)
+Start-Process -NoNewWindow -Wait $advInstallerExe ("/edit setup.aip /SetProductCode -langid 1033")
 Start-Process -NoNewWindow -Wait $advInstallerExe "/rebuild setup.aip"
