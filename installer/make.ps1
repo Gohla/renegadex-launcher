e4c008e3a8a5366c34ac0a86f6a11c98c0ade9a7
Gabriel Konat <gabrielkonat@gmail.com>
2014-03-20 22:02:39 +0100

    Don't show errors if directories don't exist.

--- installer/make.ps1
+++ installer/make.ps1
@@ -4,11 +4,11 @@ $version = $versionObj.Major.ToString() + "." + $versionObj.Minor.ToString() + "
 
 "Building installer, setting version to " + $version + "."
 
-Remove-Item -Force -Recurse "AnyCPU_ReleaseSetupFiles"
-Remove-Item -Force -Recurse "out"
-Remove-Item -Force -Recurse "setup-cache"
+Remove-Item -Force -Recurse "AnyCPU_ReleaseSetupFiles" -ea SilentlyContinue
+Remove-Item -Force -Recurse "out" -ea SilentlyContinue
+Remove-Item -Force -Recurse "setup-cache" -ea SilentlyContinue
 
 $advInstallerExe = "C:/Program Files (x86)/Caphyon/Advanced Installer 11.0/bin/x86/advinst.exe"
 
-Start-Process -NoNewWindow -Wait $advInstallerExe ("/edit setup.aip /SetVersion " + $version.ToString())
+Start-Process -NoNewWindow -Wait $advInstallerExe ("/edit setup.aip /SetVersion " + $version)
 Start-Process -NoNewWindow -Wait $advInstallerExe "/rebuild setup.aip"
